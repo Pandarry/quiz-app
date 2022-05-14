0fd932950f76d5d066af4f2f8166f15fc3298009
Bigali <104558475+Pandarry@users.noreply.github.com>
2022-05-14 19:13:43 +0600

    Initial commit

---
+++ src/components/Question.jsx
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import { QuizContext } from "../contexts/quiz";
+import Answer from "./Answer";
+
+const Question = () => {
+    const [quizState, dispatch] = useContext(QuizContext);
+    console.log ('Question', quizState)
+    const currentQuestion = quizState.questions[quizState.currentQuestionIndex];
+    return (
+        <div>
+            <div className="question">
+                {currentQuestion.question}
+            </div>
+            <div className="answers">
+                {quizState.answers.map ((answer, index) => (
+                    <Answer 
+                    key={index}
+                    index={index}
+                    answerText = {answer}
+                    correctAnswer={currentQuestion.correctAnswer}
+                    currentAnswer={quizState.currentAnswer}
+                    onSelectAnswer ={(answerText) =>
+                        dispatch({type: 'SELECT_ANSWER', payload: answerText})}/>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Question;
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,36 @@
+import React, { useContext, useReducer } from "react";
+import { QuizContext } from "../contexts/quiz";
+import Question from "./Question";
+
+
+const Quiz = () => {
+    const [quizState, dispatch] = useContext(QuizContext);
+    return (
+        <div className="quiz">
+            {quizState.showResults && (
+                <div className="results">
+                    <div className="congratulations">Congratulations</div>
+                    <div className="results-info">
+                        <div>You have complete the quiz.</div>
+                        <div>
+                            You've got {quizState.correctAnswersCount} of {' '}
+                            {quizState.questions.length}
+                         </div>
+                        <div className="next-button" onClick={() => dispatch ({type: 'Restart'})}>Restart</div>
+                    </div>
+                </div>
+            )}
+            {!quizState.showResults && (<div>
+                <div className="score">Question {quizState.currentQuestionIndex + 1}/{quizState.questions.length}</div>
+                <Question />
+                <div 
+                    className="next-button"
+                    onClick={() => dispatch ({type:'NEXT_QUESTION'})}
+                    >
+                    Next question</div>
+            </div>)}
+        </div>
+    );
+}
+
+export default Quiz;
